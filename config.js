c73e2a426fe59515c09df29723c8dbd4a4f39e7a
kenmjzuta <86815286+kenmjzuta@users.noreply.github.com>
2022-01-25 12:42:45 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Ánh em yêu ơi!',
+    introTitle: 'Yến Đinh à!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là yêu anh <33',
-    btnNo: 'Không, Anh yêu ơi :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Một ngày tốt lành nhé công chúa của anh, nhớ anh thì video call cho anh nhé.',
-    btnAccept: 'Okiiiii I love you so much <3',
+    Mà sao nhìn em lại thấy ghét vậy nè`,
+    btnIntro: 'kaka',
+    title: 'Em phải mua bim bim cho anh 😙',
+    desc: 'không thì đừng có trách ',
+    btnYes: 'chắc chắn sẽ mua <33',
+    btnNo: 'Không mua :3',
+    question:'có gì nhắn nhủ cho anh không???  <3',
+    btnReply: 'sent tin nhắn',
+    reply: 'anh ăn bim bim cho em vui :)))',
+    mess: 'Anh biết mà',
+    btnAccept: 'Okiiiii',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
